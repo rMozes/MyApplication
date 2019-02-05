b7186ca0aa66b7d26b69d63a2651412f27ab187c
rMozes <richard.mozes@thinkmobiles.com>
2019-02-05 17:16:39 +0200

    Instrumentation tests are added.

---
+++ app/src/androidTest/java/com/richi/myapplication/MainActivityTest.kt
@@ -0,0 +1,209 @@
+package com.richi.myapplication
+
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.filters.LargeTest
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import android.view.View
+import android.view.ViewGroup
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun mainActivityTest() {
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        Thread.sleep(7000)
+
+        val bottomNavigationItemView = onView(
+            allOf(
+                withId(R.id.navigation_dashboard),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.navigation),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        bottomNavigationItemView.perform(click())
+
+        val bottomNavigationItemView2 = onView(
+            allOf(
+                withId(R.id.navigation_dashboard),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.navigation),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        bottomNavigationItemView2.perform(click())
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        Thread.sleep(7000)
+
+        val bottomNavigationItemView3 = onView(
+            allOf(
+                withId(R.id.navigation_notifications),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.navigation),
+                        0
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        bottomNavigationItemView3.perform(click())
+
+        val bottomNavigationItemView4 = onView(
+            allOf(
+                withId(R.id.navigation_home),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.navigation),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        bottomNavigationItemView4.perform(click())
+
+        val bottomNavigationItemView5 = onView(
+            allOf(
+                withId(R.id.navigation_dashboard),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.navigation),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        bottomNavigationItemView5.perform(click())
+
+        val frameLayout = onView(
+            allOf(
+                withId(R.id.navigation_home),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.navigation),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        frameLayout.check(matches(isDisplayed()))
+
+        val bottomNavigationItemView6 = onView(
+            allOf(
+                withId(R.id.navigation_notifications),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.navigation),
+                        0
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        bottomNavigationItemView6.perform(click())
+
+        val bottomNavigationItemView7 = onView(
+            allOf(
+                withId(R.id.navigation_home),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.navigation),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        bottomNavigationItemView7.perform(click())
+
+        val bottomNavigationItemView8 = onView(
+            allOf(
+                withId(R.id.navigation_notifications),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.navigation),
+                        0
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        bottomNavigationItemView8.perform(click())
+
+        val bottomNavigationItemView9 = onView(
+            allOf(
+                withId(R.id.navigation_notifications),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.navigation),
+                        0
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        bottomNavigationItemView9.perform(click())
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
