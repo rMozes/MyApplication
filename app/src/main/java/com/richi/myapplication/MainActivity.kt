a43298aae0364677dbec36249f2ed1469bf48bbf
rMozes <richard.mozes@thinkmobiles.com>
2019-02-07 14:41:52 +0200

    Test

--- app/src/main/java/com/richi/myapplication/MainActivity.kt
+++ app/src/main/java/com/richi/myapplication/MainActivity.kt
@@ -25,7 +25,6 @@ class MainActivity : AppCompatActivity() {
         false
     }
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
