b84bbe9738529a4765c8fdf54cca344f8094006d
rMozes <richard@purpleocean.co>
2019-03-20 16:58:15 +0200

    Test

--- app/src/main/java/com/richi/myapplication/MainActivity.kt
+++ app/src/main/java/com/richi/myapplication/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.richi.myapplication
 
 import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
-import android.support.v7.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
